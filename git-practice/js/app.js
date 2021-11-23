e783a8494574a2df2eceb23dfd17031252a22236
GyoHeon Lee <ghl12078@gmail.com>
2021-11-23 23:52:57 +0900

    feat: copy translate js file
    hangulUglyModifier

---
+++ git-practice/js/app.js
@@ -0,0 +1,11 @@
+const input = document.querySelector(".input");
+
+output = [];
+const translate = "makeHangulUgly";
+function makeHangulUgly(event) {
+  event.preventDefault();
+  const newoutput = {
+    input: input.value,
+    output: input.value,
+  };
+}
