3f9cb11ca9bc33c1953f2d9110512a652fdf7062
GyoHeon Lee <ghl12078@gmail.com>
2021-12-02 20:16:34 +0900

    f.c course-21.12.02

---
+++ html-css-js/server.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var fs = require("fs");
+var mySet = function (request, response) {
+  var url = request.url;
+  if (request.url === "/") {
+    url = "/index.html";
+  }
+  response.writeHead(200, { "Content-Type": "text/html" });
+  var htmlFile = fs.readFileSync(__dirname + url);
+  response.end(htmlFile);
+};
+var app = http.createServer(mySet);
+app.listen(8080);
