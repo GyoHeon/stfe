4a4bf7169b83949917bb43d64dd2c9c281c11c48
GyoHeon Lee <ghl12078@gmail.com>
2021-12-03 21:45:20 +0900

    feat: make side badges

--- starbucks-clone/js/main.js
+++ starbucks-clone/js/main.js
@@ -15,3 +15,22 @@ searchInputEl.addEventListener("blur", function () {
   searchInputEl.setAttribute("placeholder", "");
   searchInputEl.value = "";
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
