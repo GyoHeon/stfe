783f115d2c997f8a41ec08ab87af7aeca08fd205
GyoHeon Lee <ghl12078@gmail.com>
2021-12-13 21:45:52 +0900

    docs: move function to getType.js

--- js-upgrade/main.js
+++ js-upgrade/main.js
@@ -1,4 +1,9 @@
-import _ from "lodash";
+import getType from "./getType.js";
 
-console.log("hello world");
-console.log(_.camelCase("hello world"));
+console.log(getType("hello world"));
+console.log(getType(123));
+console.log(getType(true));
+console.log(getType(undefined));
+console.log(getType(null));
+console.log(getType({}));
+console.log(getType([]));
