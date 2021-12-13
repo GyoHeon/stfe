ef75b5db00aa4cddf1c01a7abd584a5e4d822096
GyoHeon Lee <ghl12078@gmail.com>
2021-12-14 08:03:55 +0900

    study: variable scope

--- js-upgrade/main.js
+++ js-upgrade/main.js
@@ -1,12 +1,1 @@
-const ulEl = document.querySelector("ul");
-
-for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  li.textContent = `list-${i + 1}`;
-  if (i % 2) {
-    li.addEventListener("click", function () {
-      console.log(li.textContent);
-    });
-  }
-  ulEl.appendChild(li);
-}
+console.log(0 == "");
