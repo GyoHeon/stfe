59ee3822633e688f56bc4d75dd9a5e8fa5645b37
GyoHeon Lee <ghl12078@gmail.com>
2021-12-15 11:29:25 +0900

    feat: constructor

--- js-upgrade/main.js
+++ js-upgrade/main.js
@@ -1,1 +1,20 @@
-console.log(0 == "");
+// function User() {
+//   this.firstName = first;
+//   this.lastName = last;
+// }
+// User.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// };
+
+class User {
+  constructor(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const lgh = new User("Gyoheon", "Lee");
+console.log(lgh);
