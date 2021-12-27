b667530b346c60fbb93b15b1b63b5b26f1a8731a
GyoHeonLee <ghl12078@gmail.com>
2021-12-27 09:05:56 +0900

    course: 4.2.2

--- js-upgrade/main.js
+++ js-upgrade/main.js
@@ -1,10 +1,16 @@
 import _ from "lodash";
-import { getType } from "./getType.js";
 
-const user = { name: "lgh", age: 22, email: "ghl12078@gmail.com" };
-const copyUser = _.cloneDeep(user);
+const userA = [
+  { userId: "1", name: "lgh" },
+  { userId: "2", name: "Neo" },
+];
+const userB = [
+  { userId: "1", name: "lgh" },
+  { userId: "3", name: "Prodo" },
+];
 
-console.log(user);
-console.log(getType([1, 2, 3]));
-console.log(getType({ a: 1, b: 2 }));
-console.log(getType("b"));
+const userC = userA.concat(userB);
+console.log(userC, _.uniqBy(userC, "userId"));
+
+const userD = _.unionBy(userA, userB, "userId");
+console.log(userD);
