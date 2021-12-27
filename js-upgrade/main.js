2796ed2156a150bb46b19fbf05eae38d70ea4baa
GyoHeonLee <ghl12078@gmail.com>
2021-12-27 22:14:28 +0900

    course: 4.3.4

--- js-upgrade/main.js
+++ js-upgrade/main.js
@@ -1,11 +1,11 @@
 const str = `
-010-1234-5678
-ghl12078@gmail.com
+010-1234-5678.
+theghl12078@gmail.com.
 https://naver.com
 The lazy dog jumps over the lazy fox
-abbcccdddd
+abbcccdddd.
 `;
 
-const regExp = /the/gi;
+const regExp = /\.$/gim;
 
 console.log(str.match(regExp));
