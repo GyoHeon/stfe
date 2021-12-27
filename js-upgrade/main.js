a3c1f01f6d2c68226f85ce3f1082946dba0ebd86
GyoHeonLee <ghl12078@gmail.com>
2021-12-27 17:56:17 +0900

    course: 4.3.2

--- js-upgrade/main.js
+++ js-upgrade/main.js
@@ -1,15 +1,11 @@
-import axios from "axios";
+const str = `
+010-1234-5678
+ghl12078@gmail.com
+https://naver.com
+The lazy dog jumps over the lazy fox
+abbcccdddd
+`;
 
-const url = "https://www.omdbapi.com/?apikey=7035c60c&s=frozen";
+const regExp = /the/gi;
 
-function fetchMovies() {
-  axios.get(url).then((res) => {
-    console.log(res);
-    const h1El = document.querySelector("h1");
-    const imgEl = document.querySelector("img");
-    h1El.textContent = res.data.Search[0].Title;
-    imgEl.src = res.data.Search[0].Poster;
-  });
-}
-
-fetchMovies();
+console.log(str.match(regExp));
