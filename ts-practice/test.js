5b04dc1366f5def4f9d9d11e587be0251ccea652
GyoHeonLee <ghl12078@gmail.com>
2021-12-28 18:38:43 +0900

    course: 5.2.7

--- ts-practice/test.js
+++ ts-practice/test.js
@@ -1,6 +1,8 @@
 "use strict";
+// boolean
 let isDone = false;
 console.log(isDone);
+// number
 let decimal = 55555;
 let hex = 0xf00d;
 let binary = 0b1001;
@@ -8,6 +10,16 @@ let octal = 0o744;
 let NotANumber = NaN;
 let underscoreNum = 1000000;
 console.log(decimal, hex, binary, octal, NotANumber, underscoreNum);
+// string
 let myName = "LGH";
 let myBudget = `${underscoreNum}원 남음!`;
 console.log(myName, myBudget);
+// null, undefined
+let u = undefined;
+let n = null;
+// let myNum: number = null; impossible
+// union type
+let union = 37;
+console.log(union);
+union = null;
+console.log(union);
--- ts-practice/test.ts
+++ ts-practice/test.ts
@@ -1,6 +1,8 @@
+// boolean
 let isDone: boolean = false;
 console.log(isDone);
 
+// number
 let decimal: number = 55555;
 let hex: number = 0xf00d;
 let binary: number = 0b1001;
@@ -9,6 +11,18 @@ let NotANumber: number = NaN;
 let underscoreNum: number = 1_000_000;
 console.log(decimal, hex, binary, octal, NotANumber, underscoreNum);
 
+// string
 let myName: string = "LGH";
 let myBudget: string = `${underscoreNum}원 남음!`;
 console.log(myName, myBudget);
+
+// null, undefined
+let u: undefined = undefined;
+let n: null = null;
+// let myNum: number = null; impossible
+
+// union type
+let union: number | null = 37;
+console.log(union);
+union = null;
+console.log(union);
