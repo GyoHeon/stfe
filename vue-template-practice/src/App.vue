0083a3a7f6cd7e323098cc141458de78167b83ef
GyoHeonLee <ghl12078@gmail.com>
2021-12-30 12:39:27 +0900

    course: 9.1.6

--- vue-template-practice/src/App.vue
+++ vue-template-practice/src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <h1>{{ message }}</h1>
-  <HelloWorld />
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
 </template>
 
 <script>
-import HelloWorld from "~/components/HelloWorld.vue";
-
 export default {
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
-      message: "Hello Vue",
+      count: 0,
     };
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    },
   },
 };
 </script>
+
+<style></style>
