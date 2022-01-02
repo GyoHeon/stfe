b01993809ac865d5328402b69836215879c7b366
GyoHeonLee <ghl12078@gmail.com>
2022-01-03 07:45:55 +0900

    course: 10.4.4

--- react-template-practice/src/App.js
+++ react-template-practice/src/App.js
@@ -1,15 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Profile" component={Profile} />
-      <Route exact path="/Profile/:id" component={Profile} />
-      <Route exact path="/About" component={About} />
+      <Switch>
+        <Route path="/Profile/:id" component={Profile} />
+        <Route path="/Profile" component={Profile} />
+        <Route path="/About" component={About} />
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
