0b51ac9c4192062a303be204829fdf26670a16e8
GyoHeonLee <ghl12078@gmail.com>
2022-01-10 15:52:10 +0900

    course: 10.8.3

--- react-shadow-example/src/App.js
+++ react-shadow-example/src/App.js
@@ -1,17 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Example1 from "./components/Example1";
-import Example2 from "./components/Example2";
-import Example3 from "./components/Example3";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Example1 />
-        <Example2 />
-        <Example3 />
+        <Button />
       </header>
     </div>
   );
