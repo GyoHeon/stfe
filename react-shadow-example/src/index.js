429da8439534dd198dcea94d1cfc4910a2ab490e
GyoHeonLee <ghl12078@gmail.com>
2022-01-10 15:52:17 +0900

    course: 10.8.3

--- react-shadow-example/src/index.js
+++ react-shadow-example/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import PersonContext from "./contexts/PersonContext";
-
-const persons = [
-  { id: 0, name: "Mark", age: 39 },
-  { id: 0, name: "Hanna", age: 29 },
-];
 
 ReactDOM.render(
   <React.StrictMode>
-    <PersonContext.Provider value={persons}>
-      <App />
-    </PersonContext.Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
