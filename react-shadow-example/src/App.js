25a80200917a6e696db04a1b8da503e358889a57
GyoHeonLee <ghl12078@gmail.com>
2022-01-11 01:23:24 +0900

    course: 10.9.2

--- react-shadow-example/src/App.js
+++ react-shadow-example/src/App.js
@@ -1,44 +1,76 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./components/modal";
 
-const Person = React.memo(({ name, age }) => {
-  console.log("Person render");
-  return (
-    <div>
-      {name} / {age}
-    </div>
-  );
-});
+// const Person = React.memo(({ name, age }) => {
+//   console.log("Person render");
+//   return (
+//     <div>
+//       {name} / {age}
+//     </div>
+//   );
+// });
+
+// function App() {
+//   const [state, setState] = React.useState({
+//     text: "",
+//     persons: [
+//       { id: 1, name: "Mark", age: 39 },
+//       { id: 2, name: "Hanna", age: 29 },
+//     ],
+//   });
+
+//   const { text, persons } = state;
+
+//   return (
+//     <div>
+//       <input type="text" value={text} onChange={change} />
+//       <ul>
+//         {persons.map((person) => (
+//           <Person {...person} key={person.id} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+
+//   function change(e) {
+//     setState({
+//       ...state,
+//       text: e.target.value,
+//     });
+//   }
+// }
 
 function App() {
-  const [state, setState] = React.useState({
-    text: "",
-    persons: [
-      { id: 1, name: "Mark", age: 39 },
-      { id: 2, name: "Hanna", age: 29 },
-    ],
-  });
+  const [visible, setVisible] = useState(false);
 
-  const { text, persons } = state;
+  const open = () => {
+    setVisible(true);
+  };
+  const close = () => {
+    setVisible(false);
+  };
 
   return (
     <div>
-      <input type="text" value={text} onChange={change} />
-      <ul>
-        {persons.map((person) => (
-          <Person {...person} key={person.id} />
-        ))}
-      </ul>
+      <button onClick={open}>open</button>
+      {visible && (
+        <Modal>
+          <div
+            style={{
+              width: "100px",
+              height: "100px",
+              backgroundColor: "tomato",
+            }}
+            onClick={close}
+          >
+            Hello
+          </div>
+        </Modal>
+      )}
     </div>
   );
-
-  function change(e) {
-    setState({
-      ...state,
-      text: e.target.value,
-    });
-  }
 }
 
 export default App;
