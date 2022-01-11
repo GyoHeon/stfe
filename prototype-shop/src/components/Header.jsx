fafcddfbbd42e7e09b0ac62ca08bd18eb9b875ea
GyoHeonLee <ghl12078@gmail.com>
2022-01-11 15:35:08 +0900

    course: 10.11.3

---
+++ prototype-shop/src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="header__container">
+        <div className="title">Awesome Prototype in shop</div>
+        <div className="subtitle">
+          Check out what other designers have created using PrototPie-download
+          these examples to learn exactly how they made their interactions.
+        </div>
+        <div className="btn_area">
+          <a href="https://www.protopie.io" target="_blank" rel="noreferrer">
+            <button>Try ProtoPie Yourself</button>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
