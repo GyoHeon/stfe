61557c14c7779ddcf8e4a5aa1fd64eb6c6334407
GyoHeonLee <ghl12078@gmail.com>
2022-01-22 06:44:57 +0900

    course: 3.2

--- styled-components-example/src/App.tsx
+++ styled-components-example/src/App.tsx
@@ -1,53 +1,11 @@
-import styled, { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-  background-color: ${(props) => props.theme.backgroundColor};
-  width: 300px;
-  height: 300px;
-  display: flex;
-`;
-
-const rotateAnimation = keyframes`
-0%{
-  transform: rotate(0deg);
-  border-radius: 0px;
-}
-50%{
-  transform: rotate(180deg);
-  border-radius: 35%;
-}
-100%{
-  transform: rotate(360deg);
-  border-radius: 0px;
-}`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAnimation} 5s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 100px;
-    }
-  }
-`;
+import Circle from "./Circle.tsx";
 
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <Emoji>😀</Emoji>
-      </Box>
-      <Emoji>😀</Emoji>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
