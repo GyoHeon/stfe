e67ec539feb4b3fd760205d330c5355b57e01b69
GyoHeonLee <ghl12078@gmail.com>
2022-01-24 07:12:27 +0900

    course: 3.7

--- styled-components-example/src/App.tsx
+++ styled-components-example/src/App.tsx
@@ -8,10 +8,24 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>;
+}
+
 function App() {
+  const onClick = (e: React.FormEvent<HTMLButtonElement>) => {};
+
   return (
     <Container>
-      <H1>protected</H1>
+      <Dummy text="hahaha" active />
+      <form>
+        <button onClick={onClick}>Click ME</button>
+      </form>
     </Container>
   );
 }
