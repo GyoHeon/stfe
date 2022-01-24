d6a0a1f589b0c1928a7bc62916b3b47620921db2
GyoHeonLee <ghl12078@gmail.com>
2022-01-24 18:53:49 +0900

    docs: create new project

--- styled-components-example/src/index.tsx
+++ styled-components-example/src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App.tsx";
-import { lightTheme, darkTheme } from "./theme.ts";
+import { theme } from "./theme.ts";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
