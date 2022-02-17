73be50628608908e82203b8e6c343a60c6f2e62c
GyoHeonLee <ghl12078@gmail.com>
2022-02-17 17:30:14 +0900

    course: 249

--- 01-starting-project/src/store/index.js
+++ 01-starting-project/src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 export const INCREMENT = "increment";
 export const INCREASE = "increase";
 export const DECREMENT = "decrement";
