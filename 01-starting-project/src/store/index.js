208bfe10a188b90279def223ac2c1e40a978d819
GyoHeonLee <ghl12078@gmail.com>
2022-02-17 17:50:31 +0900

    course: 250

--- 01-starting-project/src/store/index.js
+++ 01-starting-project/src/store/index.js
@@ -1,9 +1,13 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+export const INCREMENT = "increment";
+export const INCREASE = "increase";
+export const DECREMENT = "decrement";
+export const TOGGLE = "toggle";
+
+const counterSlice = createSlice({
   name: "counter",
   initialState: initialState,
   reducers: {
@@ -22,41 +26,6 @@ createSlice({
   },
 });
 
-export const INCREMENT = "increment";
-export const INCREASE = "increase";
-export const DECREMENT = "decrement";
-export const TOGGLE = "toggle";
-
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({ reducer: counterSlice.reducer });
 
 export default store;
