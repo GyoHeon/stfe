9b0833cee5687f8a01bc7617c7a370cbe97bb314
GyoHeonLee <ghl12078@gmail.com>
2022-03-01 08:01:43 +0900

    course: 6.1

--- React/trello-clone/src/App.js
+++ React/trello-clone/src/App.js
@@ -1,12 +1,14 @@
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { minuteState, hourSelector } from "./atoms";
-import logo from "./logo.svg";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (e) => {
-    setMinutes(e.currentTarget.value);
+    setMinutes(+e.currentTarget.value);
+  };
+  const onHoursChange = (e) => {
+    setHours(+e.currentTarget.value);
   };
 
   return (
@@ -17,7 +19,12 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
