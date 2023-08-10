3f8ad5f978b4aaad8aca834586c5483020a46fdc
Gyoheon <ghl12078@gmail.com>
2023-08-10 17:39:30 +0900

    Course: 6.7

--- Back/node/index.ts
+++ Back/node/index.ts
@@ -3,13 +3,22 @@ import { createServer } from "http";
 const PORT = 3000;
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
+  if (req.url === "/home") {
+    res.writeHead(200, { "Content-Type": "application/json" });
 
-  res.end(
-    JSON.stringify({
-      message: "Hello World",
-    })
-  );
+    res.end(
+      JSON.stringify({
+        message: "Hello World",
+      })
+    );
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.write("<html><body><h1>About</h1></body></html>");
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () =>
