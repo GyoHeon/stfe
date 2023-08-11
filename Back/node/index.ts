adc063653b3b1c856319d75dc495b188777f038b
GyoHeonLee <ghl12078@gmail.com>
2023-08-12 03:38:58 +0900

    Course: 6.1.8

--- Back/node/index.ts
+++ Back/node/index.ts
@@ -1,23 +1,29 @@
 import { createServer } from "http";
 
 const PORT = 3000;
+const targetObject = { a: 1, b: 2, c: 3 };
 
 const server = createServer((req, res) => {
-  if (req.url === "/home") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-
-    res.end(
-      JSON.stringify({
-        message: "Hello World",
-      })
-    );
-  } else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-
-    res.write("<html><body><h1>About</h1></body></html>");
+  if (req.method === "POST" && req.url === "/home") {
+    req.on("data", (data) => {
+      console.log(data);
+      const stringfiedData = data.toString();
+      console.log(stringfiedData);
+      Object.assign(targetObject, JSON.parse(stringfiedData));
+    });
   } else {
-    res.statusCode = 404;
-    res.end();
+    if (req.url === "/home") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+
+      res.end(JSON.stringify(targetObject));
+    } else if (req.url === "/about") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+
+      res.write("<html><body><h1>About</h1></body></html>");
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
   }
 });
 
