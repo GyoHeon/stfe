3522b58446a9087c031573e27f0470af29ec621a
Gyoheon <ghl12078@gmail.com>
2023-09-05 18:27:46 +0900

    Feat: No collapse when start

--- project-design/ya-fa/src/components/SideBar.tsx
+++ project-design/ya-fa/src/components/SideBar.tsx
@@ -1,20 +1,23 @@
 import { Menu } from "antd";
 import Sider from "antd/es/layout/Sider";
 import { SIDE_ITEMS, SIDE_NAV } from "../constants/layout";
+import { useItem } from "../hooks/useItem";
 import { usePath } from "../hooks/usePath";
 
 export function SideBar() {
   const path = usePath();
+  const item = useItem();
   const menus = SIDE_ITEMS[path];
 
   return path.length > 0 ? (
     <Sider width={200}>
       <Menu
         mode="inline"
-        defaultSelectedKeys={[menus[0].title, menus[0].sub[0].title]}
-        openKeys={[menus[0].title]}
+        defaultSelectedKeys={[menus[0].title, item || menus[0].sub[0].title]}
+        openKeys={menus.map(({ title }) => title)}
         style={{ height: "100%" }}
         items={SIDE_NAV(path)}
+        selectedKeys={[item || menus[0].sub[0].title]}
       />
     </Sider>
   ) : (
--- project-design/ya-fa/src/components/Wiki.tsx
+++ project-design/ya-fa/src/components/Wiki.tsx
@@ -1,19 +1,21 @@
 import { DocumentData, doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { useItem } from "../hooks/useItem";
 import { db } from "../utils/firebase";
 
 export function Wiki() {
+  const item = useItem();
   const [document, setDocument] = useState<DocumentData | undefined>();
   const date = new Date((document?.date?.seconds || 0) * 1000);
 
   useEffect(() => {
     const querySnapshot = async () => {
-      const data = await getDoc(doc(db, "wiki", "rules"));
+      const data = await getDoc(doc(db, "wiki", item || "rules"));
 
       setDocument(data.data());
     };
     querySnapshot();
-  }, []);
+  }, [item]);
 
   return (
     <div>
