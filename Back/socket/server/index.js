1ade6dde1f51840a57317017a67aee0dcf2e2165
GyoHeonLee <ghl12078@gmail.com>
2023-10-13 01:29:36 +0900

    Course: WebSocket.1.4

---
+++ Back/socket/server/index.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 7071 });
+
+wss.on("connection", (ws) => {
+  ws.send("connected");
+
+  ws.on("message", (messageFromClient) => {
+    const message = JSON.parse(messageFromClient);
+    console.log(message);
+  });
+});
