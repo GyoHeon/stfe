ab968233a653ad4091bb2e3b225186a18e8139d0
GyoHeonLee <ghl12078@gmail.com>
2023-10-14 18:09:42 +0900

    Course: WebSocket

--- Back/express-socket/src/index.js
+++ Back/express-socket/src/index.js
@@ -1,12 +1,32 @@
 import express from "express";
+import { createServer } from "http";
+import path from "path";
+import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("socket id:", socket.id);
+
+  socket.on("join", (message) => {
+    console.log("join", { message });
+  });
+  socket.on("sendMessage", (message) => {
+    console.log("sendMessage", { message });
+  });
+  socket.on("disconnect", (message) => {
+    console.log("disconnect", { message });
+  });
+});
 
 const PORT = 4000;
 
 const PUBLIC_PATH = "../public";
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, PUBLIC_PATH)));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`✅ Server is listening on port ${PORT}`);
 });
