ae21c90744bf8a17aa5d8db575b9b59d14542ec5
GyoHeonLee <ghl12078@gmail.com>
2023-10-22 21:42:59 +0900

    Course: Auth

--- Back/auth/index.js
+++ Back/auth/index.js
@@ -21,7 +21,20 @@ const posts = [
   { username: "lgh", title: "Post 2" },
 ];
 
-const authMiddleware = (req, res, next) => {};
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.warn(err);
+    }
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
 app.get("/posts", authMiddleware, (req, res) => {
   res.json(posts);
