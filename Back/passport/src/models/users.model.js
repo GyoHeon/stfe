62c734848e78e9188b703963d423c9d989d2b6fb
GyoHeonLee <ghl12078@gmail.com>
2023-10-29 15:23:06 +0900

    Feat: Reset

---
+++ Back/passport/src/models/users.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  googleId: {
+    type: String,
+    unique: true,
+  },
+});
+
+export const User = mongoose.model("user", userSchema);
---
+++ Back/passport/src/server.js
@@ -0,0 +1,31 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+
+dotenv.config({ path: ".env" });
+
+const app = express();
+
+const PUBLIC_PATH = "../public";
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, PUBLIC_PATH)));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.MONGO_ID)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`✅ Server Start PORT : ${PORT}`);
+});
