f6303a77446113db31d24d33de1119cec82e914e
GyoHeonLee <ghl12078@gmail.com>
2024-07-31 16:26:32 +0900

    fix: fix text

--- python/study/api/migrate_db.py
+++ python/study/api/migrate_db.py
@@ -8,5 +8,5 @@ def reset_database():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
-if __name__ == "__main":
+if __name__ == "__main__":
     reset_database()
