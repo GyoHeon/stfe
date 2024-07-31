8cf6fec9dd1c38ffff9af21f1736b35c711c10a4
GyoHeonLee <ghl12078@gmail.com>
2024-07-31 12:04:06 +0900

    feat: add param

--- python/study/api/schemas/task.py
+++ python/study/api/schemas/task.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel, Field
 
-class Task(BaseModel):
-    id: int
+
+class TaskBase(BaseModel):
     title: str | None = Field(None, example="세탁소에 맡긴 것을 찾으러 가기")
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="완료 플래그")
+
+    class Config:
+        orm_mode = True
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+    
+    class Config:
+        orm_mode = True
